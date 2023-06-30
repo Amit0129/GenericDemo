23e49ceddf10c8ce23bd1715fec6c768ddad09b1
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-06-30 08:13:45 +0530

    [Amit] Generic Method and class Examples

---
+++ GenericsDemo/GenericsDemo/Generics.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsDemo
+{
+    //Generic means General form 
+    //It dose not Specify a perticular Data type
+    //We are using <> (Angular barket) with type name.
+    //TypeName <T> T is your type parameater
+    //Generic Allow us to a single class or method that can be used in a different Datatype.
+    // Advantage - We can reuse the code.
+    public class Generics<T>
+    {
+        //public string name;
+        public T data;
+        public Generics(T data)
+        {
+            this.data = data;
+            Console.WriteLine(this.data);
+        }
+        public void GenericMethod(T num)
+        {
+            Console.WriteLine(num);
+        }
+       
+    }
+    public class GenericMethod
+    {
+        public void Generic<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+        public void GenericMethodOne<T>(T num)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ GenericsDemo/GenericsDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace GenericsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int> generics = new Generics<int>(25);
+            Generics<string> genericsString = new Generics<string>("Amit");
+            Generics<double> genericsDouble = new Generics<double>(0.19);
+            Generics<char> genericsChar = new Generics<char>('a');
+            Generics<bool> genericsBool = new Generics<bool>(true);
+            Generics<float> genericsFloat = new Generics<float>(1.2f);
+            Generics<long> genericsLong = new Generics<long>(5);
+            Console.WriteLine("=========================================================");
+            generics.GenericMethod(9);
+            genericsString.GenericMethod("Nayak");
+            genericsDouble.GenericMethod(1.59);
+            genericsChar.GenericMethod('m');
+            genericsBool.GenericMethod(false);
+            Console.WriteLine("==========================================================");
+            GenericMethod obj = new GenericMethod();
+            obj.Generic<int>(30);
+            obj.Generic<string>("Kumar");
+
+        }
+    }
+}
